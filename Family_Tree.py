81b06d9122cb074ac36c3064b1a0a9f5e95478c0
Danny Marshall <35569463+Dharoldm@users.noreply.github.com>
2018-05-25 18:45:04 -0400

    Added Family Tree Class
    This will be the object that creates persons and adds children and decides how people are related in the map.

--- Family_Tree.py
+++ Family_Tree.py
@@ -1,2 +1,19 @@
-import Person.py
-import Graph.py
+from Person import Person
+from Graph import Graph
+
+class FamilyTree(object):
+
+    def __init__(self, house, head=None):
+        self.house = house
+        self.tree = Graph()
+        if head:
+            self.tree.adj_map[head.name] = head
+            self.tree.size +=1
+
+    def __repr__(self):
+        for i in self.tree.adj_map:
+            return i
+Eric = Person("Eric", 198, "male")
+Tree = FamilyTree("Hawick", Eric)
+print(Tree.tree.size)
+print(Tree)
--- Person.py
+++ Person.py
@@ -1,9 +1,13 @@
 class Person(object):
-    def __init__(self, name, birth, gender, legitimacy = "Trueborn"):
+    def __init__(self, name, birth, gender, legitimacy="Trueborn"):
         self.birth = birth
         self.name = name
         self.gender = gender
         self.legitimacy = legitimacy
+        self.children = []
+
+    def __repr__(self):
+        return f"{self.name}"
 
     def age(self, year):
         return year - self.birth
